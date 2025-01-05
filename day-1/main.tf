90645c73941091aad735404099093269e2efc9f8
Kumar <kumar.multicloud@gmail.com>
2025-01-05 16:31:13 +0530

    az removed

--- day-1/main.tf
+++ day-1/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "dev" {
   ami = var.ami_id
   instance_type = var.instancetype
   key_name = var.keyname
-  availability_zone = "ap-south-1a"
   tags = {
     Name = var.ec2name
   }
