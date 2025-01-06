96ce9a5f9adeb2c960ae3b05c4b70a218cd5cbb7
Kumar <kumar.multicloud@gmail.com>
2025-01-06 12:55:30 +0530

    separated day-3-statefile and day-3-statefile-s3

---
+++ day-3-statefile-s3/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "dev" {
+  ami = "ami-0fd05997b4dff7aac"
+  instance_type = "t2.micro"
+  key_name = "multicloud-keypair"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "StateFileInS3"
+  }
+}
